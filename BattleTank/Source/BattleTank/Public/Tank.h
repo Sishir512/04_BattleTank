3be00b46bbfb2e0520de4d4dc0f378cd012d36cf
Sishir512 <sishir.wagle512@gmail.com>
2020-06-04 12:41:26 +0545

    A different aiming component has been added (inherits from actor component) and AimAt function is placed there.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -14,12 +15,15 @@ class BATTLETANK_API ATank : public APawn
 public:
 	void AimAt(FVector HitLocation);
 
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 
 private:
 	// Sets default values for this pawn's properties
 	ATank();
 
-protected:
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
