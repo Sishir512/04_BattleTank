e4b431882b243db6dbdbd0b886e470132dfd4211
Sishir512 <sishir.wagle512@gmail.com>
2020-06-16 12:45:53 +0545

    In this section we have added a Spring Wheel to the tank in order to solve the inappropriate behavior of the tank that we face while driving it . We have added Four spawn points for the wheels to spawn at the tank . All in all we have added a suspension at the tank . The wheel is a USphereComponent .

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "Components/SphereComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "SprungWheel.h"
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassWheelConstraint"));
+	SetRootComponent(MassWheelConstraint);
+	Axle = CreateDefaultSubobject<USphereComponent>(FName("Axle"));
+	Axle->SetupAttachment(MassWheelConstraint);
+
+
+	/*Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
+	Mass->SetupAttachment(MassWheelConstraint);*/
+
+	Wheel = CreateDefaultSubobject<USphereComponent>(FName("Wheel"));
+	Wheel->SetupAttachment(Axle);    //Can be used only in constructer
+
+
+	AxleWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("AxleWheelConstraint"));
+	AxleWheelConstraint->SetupAttachment(Axle);
+
+
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetupConstraint();
+
+
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASprungWheel::SetupConstraint() {
+	if (!GetAttachParentActor()) { return; }
+	UPrimitiveComponent* BodyRoot = Cast<UPrimitiveComponent>(GetAttachParentActor()->GetRootComponent());
+	if (!BodyRoot) { return; }
+	MassWheelConstraint->SetConstrainedComponents(BodyRoot, NAME_None, Axle, NAME_None);
+	AxleWheelConstraint->SetConstrainedComponents(Wheel, NAME_None, Axle, NAME_None);
+}
